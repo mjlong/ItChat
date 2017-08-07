01123824d781c011cc872050eaaea5981bee0731
Jilang_athena <jlmiao@mit.edu>
2017-08-06 23:47:01 -0400

    the 3 files to run

---
+++ main/chat1.py
@@ -0,0 +1,8 @@
+import itchat
+ses1 = itchat.new_instance();
+ses1.auto_login(hotReload=True, statusStorageDir='ses1jlmiao.pkl');
+
+ses1.run();
+
+
+
---
+++ main/chat2.py
@@ -0,0 +1,8 @@
+import itchat
+ses2 = itchat.new_instance();
+ses2.auto_login(hotReload=True, statusStorageDir='ses1jlmiao.pkl');
+
+ses2.runsend();
+
+
+
