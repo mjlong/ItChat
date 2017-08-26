f9b1111dbe49b3a3738c5ae330bd56c513767f6f
Jilang Miao 缪佶朗@office <jlmiao@mit.edu>
2017-08-26 17:06:36 -0400

    working receive()

--- itchat/utilsgmail.py
+++ itchat/utilsgmail.py
@@ -1,8 +1,19 @@
 import os
+import sys
+import io
+from itertools import chain
+
 import smtplib
+import email
+import imaplib
+
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+
+
+import email.header
+
 
 def file2dict(fname):
     f = open(os.environ[fname],'r');
@@ -14,9 +25,44 @@ def file2dict(fname):
     f.close();
     return emailcrd;
 
+def search_string(uid_max, criteria):
+    c = list(map(lambda t: (t[0], '"'+str(t[1])+'"'), \
+                 criteria.items())) \
+        + [('UID', '%d:*' % (uid_max+1))]
+    return '(%s)' % ' '.join(chain(*c))
+    # Produce search string in IMAP format:
+    #   e.g. (FROM "me@gmail.com" SUBJECT "abcde" BODY "123456789" UID 9999:*)
+
+def breakmultipart(msg):
+    rv = [];
+    for part in msg.get_payload():
+        ptype = part.get_content_maintype();
+        if(ptype != 'multipart'):
+            rv.append(part);
+        else:
+            rv+=breakmultipart(part);
+    return rv;
+
+import random
+import string 
+def id_generator(size=8, chars=string.ascii_uppercase+string.digits):
+    return ''.join(random.choice(chars) for _ in range(size));
+
+import time
+import datetime
+dt =  datetime.datetime 
+def ts_generator():
+    return dt.strftime(dt.now(),'%y%m%d-%H%M%S%f');
+
+def nm_generator():
+    return '%s_%s'%(ts_generator(),id_generator());
+
+
 class mygmail:
     def __init__(self):
         self.emailcrd = file2dict('SESCRED');
+        self.emaildb  = os.environ['EMAILDB'];
+        self.downdir  = os.environ['DOWNDIR'];
 
     def send_txt(self,targets,subject,txtmsg):
         msg = MIMEText(txtmsg);
@@ -53,3 +99,91 @@ class mygmail:
         server.login(self.emailcrd['USER'],self.emailcrd['PASSWORD']);
         server.sendmail(self.emailcrd['SENDER'],targets,msg.as_string());
         server.quit();
+
+    def get_first_text_block(self,user,msg):
+        type = msg.get_content_maintype()
+        text = None;
+        if type == 'multipart':
+            parts = breakmultipart(msg);
+            for part in parts:
+                print(part.get_content_maintype());
+                if(part.get_content_maintype() == 'text'):
+                    if(None==text):
+                        text =  part.get_payload(decode=True).\
+                                decode(part.get_content_charset());
+                        fname = nm_generator();
+                        with io.open(self.emaildb+fname,'w',encoding='utf-8') as f:
+                            f.write((user+'\n').decode('utf-8'));
+                            f.write(text);
+    
+                if(part.get_content_maintype() == 'image'):
+                    fname = self.emaildb+nm_generator();
+                    dname = '%s%s.%s' % (self.downdir,nm_generator(),'png');
+                    open(dname,'wb').write(part.get_payload(decode=True));
+                    with open(fname,'w') as f:
+                        f.write(user+'\n');
+                        f.write(dname);
+
+    
+    
+            #for a pure text message sent by gmail
+            #part 0 is 'text', the content is the text itself
+            #part 1 is 'text', the content is html element containing the text
+        elif type == 'text':
+            text =  msg.get_payload(decode=True).decode(msg.get_content_charset());
+        if(None==text):
+            rv = None;
+        else:
+            rv = text.encode('utf-8');
+        #convert <type 'unicode'> to <type 'str'>
+
+
+
+    def receive(self):
+        server = imaplib.IMAP4_SSL(self.emailcrd['REC_HOST'],self.emailcrd['REC_PORT']);
+        server.login(self.emailcrd['USER'],self.emailcrd['PASSWORD']);
+        server.select("INBOX");
+
+        criteria = {
+            'FROM':    'miaojilang@gmail.com',
+            'SUBJECT': 'wechat'
+        }
+
+        uid_max = 0;
+#Initialize `uid_max`. Any UID less than or equal to `uid_max` will be ignored subsequently
+        rv,data = server.uid('search', None, search_string(uid_max, criteria))
+        uids = [int(s) for s in data[0].split()]
+        if uids:
+            uid_max = max(uids)
+        server.logout()
+# Have to login/logout each time because that's the only way to get fresh results.
+        while 1:
+            server = imaplib.IMAP4_SSL(self.emailcrd['REC_HOST'],self.emailcrd['REC_PORT']);
+            server.login(self.emailcrd['USER'],self.emailcrd['PASSWORD']);
+            server.select("INBOX");
+            rv,data = server.uid('search', None, search_string(uid_max, criteria));
+            uids = [int(s) for s in data[0].split()]
+            for uid in uids:
+                # Have to check again because Gmail sometimes does not obey UID criterion.
+                if uid > uid_max:
+                    rv, data = server.uid('fetch', uid, '(RFC822)')  # fetch entire message
+                    msg = email.message_from_string(data[0][1]);
+                    uid_max = uid;
+                    subj = msg['Subject'];
+                    ind1 = subj.find('(');
+                    ind2 = subj.find(')');
+                    username = subj[ind1+1:ind2].replace('#','@');
+                    print(type(subj));
+                    print('New message from ',username);
+                    self.get_first_text_block(username,msg);
+
+            
+            server.logout();
+            time.sleep(0.1);
+
+
+
+if __name__ == "__main__":
+    g = mygmail()
+    #g.send_txtimg(None,'sub:hehe','txt:haha','/home/jlmiao/Dropbox (MIT)/source/funGitHub/ItChat/V144.png')
+    g.receive()
