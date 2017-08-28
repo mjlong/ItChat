48b3cfe8f82d6a9d06a85ebca5b3bb899c269b99
Jilang Miao 缪佶朗@office <jlmiao@mit.edu>
2017-08-27 22:45:14 -0400

    disable cmdQR, use QR.png sent by email

--- main/chat0.py
+++ main/chat0.py
@@ -1,8 +1,8 @@
 import itchat
 ses = itchat.new_instance();
-ses.auto_login(enableCmdQR=1,hotReload=True, statusStorageDir='ses1jlmiao.pkl');
+ses.auto_login(hotReload=True, statusStorageDir='ses1jlmiao.pkl');
 
-fg = ses.search_chatrooms(name="fbgrp")[0];
-fg.send('successfully logged on athena.dialup.mit.edu');
+fg = ses.search_chatrooms(name="Testgroup1")[0];
+fg.send('successfully logged on desktop');
 
 
