aa1674d977c5b8ac5a454ba6cc3ed55fb92e0d8b
Jilang Miao 缪佶朗@office <jlmiao@mit.edu>
2017-09-02 16:29:10 -0400

    take cmd argument to specify user

--- main/chat0.py
+++ main/chat0.py
@@ -1,8 +1,9 @@
 import itchat
 ses = itchat.new_instance();
-ses.auto_login(hotReload=True, statusStorageDir='ses1jlmiao.pkl');
+ses.auto_login(hotReload=True, statusStorageDir='ses1helper.pkl');
 
-fg = ses.search_chatrooms(name="Testgroup1")[0];
-fg.send('successfully logged on desktop');
+
+#fg = ses.search_chatrooms(name="Testgroup1")[0];
+#fg.send('successfully logged on desktop');
 
 
