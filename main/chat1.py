09f9da4c062728f499a1f05dae441d8da66c3d8b
JL@VMWare <miaojilang@gmail.com>
2017-12-16 09:15:57 -0500

    dont forward to gmail the group messages that Ill forward

--- main/chat1.py
+++ main/chat1.py
@@ -1,7 +1,5 @@
+import sys
 import itchat
 ses = itchat.new_instance();
-ses.auto_login(hotReload=True, statusStorageDir='ses1jlmiao.pkl');
-ses.run();
-
-
-
+ses.auto_login(hotReload=True, statusStorageDir=sys.argv[1]+'.pkl');
+ses.run(fwemail=False);
